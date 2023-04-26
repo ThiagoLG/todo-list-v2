6a46043900b12d411ef59e09be685114092024ff
ThiagoLG <thiago.lg53@gmail.com>
2023-04-26 00:00:19 -0300

    Created project base (project + eslint + styled components + google font)

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import { HomeContainer } from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form>
+        <input type="text" />
+        <button type="submit">Enviar</button>
+      </form>
+    </HomeContainer>
+  )
+}
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,3 @@
+import styled from 'styled-components'
+// export const HomeContainer
+export const HomeContainer = styled.main``
