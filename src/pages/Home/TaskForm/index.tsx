8990dc3bb011d4425381efa2ba23f348f5162f11
ThiagoLG <thiago.lg53@gmail.com>
2023-04-29 17:02:21 -0300

    Expanding task creation form with new fields

--- src/pages/Home/TaskForm/index.tsx
+++ src/pages/Home/TaskForm/index.tsx
@@ -1,33 +1,100 @@
-import { Autocomplete, Box, Paper, TextField } from '@mui/material'
+import {
+  Autocomplete,
+  Box,
+  Button,
+  Chip,
+  Paper,
+  TextField,
+} from '@mui/material'
 import { FormGroup, SelectOptionIndicator, TaskFormContainer } from './styles'
+import TaskAltSharpIcon from '@mui/icons-material/TaskAltSharp'
+import { useState } from 'react'
+
+interface CategoryOptions {
+  id: number
+  color: string
+  value: string
+}
+
+interface TaskTagItem {
+  id: number
+  value: string
+  color: string
+  category: string
+}
 
 export function TaskForm() {
-  const categories = [
+  const tagsList: TaskTagItem[] = [
+    {
+      id: 1,
+      value: 'Front-end',
+      color: '#b53f3f',
+      category: 'Front-end',
+    },
+    {
+      id: 2,
+      value: 'Back-end',
+      color: '#3f51b5',
+      category: 'Back-end',
+    },
+    {
+      id: 3,
+      value: 'Mobile',
+      color: '#00ff26',
+      category: 'Mobile',
+    },
+    {
+      id: 4,
+      value: 'Design',
+      color: '#3f51b5',
+      category: 'Design',
+    },
+    {
+      id: 5,
+      value: 'DevOps',
+      color: '#ff00ae',
+      category: 'DevOps',
+    },
+  ]
+  const categories: CategoryOptions[] = [
     {
       id: 1,
       color: 'red',
-      value: 'Compras',
+      value: 'Shopping',
     },
     {
       id: 2,
       color: 'green',
-      value: 'Estudos',
+      value: 'Studies',
     },
     {
       id: 3,
       color: 'blue',
-      value: 'Compras',
+      value: 'Entertrainment',
+    },
+    {
+      id: 4,
+      color: 'black',
+      value: 'Other',
     },
   ]
+  const [tags, setTags] = useState<TaskTagItem[]>(tagsList)
 
   return (
     <div className="formContainer">
       <TaskFormContainer>
-        <Paper elevation={3}>
+        <Paper
+          elevation={3}
+          sx={{
+            'margin-top': '1rem',
+            padding: '1rem',
+          }}
+        >
           <div className="row">
             {/* Category Field */}
             <FormGroup size={5}>
               <Autocomplete
+                aria-required
                 id="taskCategory"
                 fullWidth
                 options={categories}
@@ -44,7 +111,7 @@ export function TaskForm() {
                     label="Select a Category"
                     inputProps={{
                       ...params.inputProps,
-                      autoComplete: 'new-password',
+                      autoComplete: 'Category',
                     }}
                     variant="standard"
                   />
@@ -65,16 +132,94 @@ export function TaskForm() {
             </FormGroup>
           </div>
 
-          {/*  Task Description */}
           <div className="row">
+            {/* Address */}
+            <FormGroup size={10}>
+              <TextField id="task-address" label="Address" variant="standard" />
+            </FormGroup>
+          </div>
+
+          <div className="row">
+            {/* Price */}
+            <FormGroup size={4}>
+              <TextField
+                id="task-medium-price"
+                label="Task Medium Price"
+                variant="standard"
+              />
+            </FormGroup>
+            {/* WeekDays opened */}
+            <FormGroup size={3}>
+              <TextField
+                id="task-week-days"
+                label="Weekdays Opened"
+                variant="standard"
+              />
+            </FormGroup>
+            {/* Opening Hours */}
+            <FormGroup size={3}>
+              <TextField
+                id="task-opening-hours"
+                label="Opening Hours"
+                variant="standard"
+              />
+            </FormGroup>
+          </div>
+
+          <div className="row">
+            {/*  Task Description */}
             <FormGroup size={10}>
               <TextField
                 id="task-description"
                 label="Task Description"
                 multiline
-                rows={3}
+                rows={5}
                 variant="standard"
               />
+            </FormGroup>
+          </div>
+
+          <div className="row">
+            {/*  Tags */}
+            <FormGroup size={10}>
+              <Autocomplete
+                id="task=tags"
+                multiple
+                options={tags}
+                renderTags={(tagValue, getTagProps) => {
+                  return tagValue.map((option, index) => (
+                    <Chip
+                      {...getTagProps({ index })}
+                      label={option.value}
+                      key={option.value}
+                      sx={{
+                        backgroundColor: option.color,
+                      }}
+                    />
+                  ))
+                }}
+                getOptionLabel={(option) => option.value}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    placeholder="Add Tag"
+                    variant="standard"
+                  />
+                )}
+              ></Autocomplete>
+            </FormGroup>
+          </div>
+
+          <div className="row">
+            {/*  Task Description */}
+            <FormGroup size={10}>
+              <Button
+                variant="outlined"
+                color="success"
+                startIcon={<TaskAltSharpIcon />}
+              >
+                Create Task
+              </Button>
             </FormGroup>
           </div>
         </Paper>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,29 @@
+import { Box, Button } from '@mui/material'
+import { useState } from 'react'
 import { HomeContainer } from './styles'
 import { TaskForm } from './TaskForm'
 
 export function Home() {
+  const [showForm, setShowForm] = useState(false)
   return (
     <HomeContainer>
-      <TaskForm />
+      <Box sx={{ flex: 1, textAlign: 'center' }}>
+        <Button
+          variant="outlined"
+          sx={{ margin: 'auto' }}
+          onClick={() => {
+            setShowForm((show) => !show)
+          }}
+        >
+          Add new task
+        </Button>
+      </Box>
+
+      <Box
+        sx={{ transition: '200ms', transform: `scale(${showForm ? 1 : 0})` }}
+      >
+        <TaskForm />
+      </Box>
     </HomeContainer>
   )
 }
